b8c2bd90d84b750616c5c24ba984ad3308c7f67c
Daniel Fagbuyi <Daniel.fagbuyi@icloud.com>
2017-09-11 08:27:55 -0700

    update

---
+++ Hello.java
@@ -0,0 +1,10 @@
+
+public class Hello {
+    public stacic void main(String[] args){
+
+	System.out.println("This is a statement from the red leader");
+       	System.out.println("This is a  another statement from the red leader");
+
+    }
+
+}
